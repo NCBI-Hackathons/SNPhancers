ac1e5e620e19275916e612a92682a0ff13d9239f
Sean La <seanla1996@gmail.com>
2017-07-24 12:39:37 -0700

    improved usability of get_gwas_snp_accessions.sh

--- src/get_gwas_snp_accessions.sh
+++ src/get_gwas_snp_accessions.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+if [ "$#" -ne 2 ]; then
+	echo "Description: Retrieves accessions numbers for integenic SNPs from GWAS catalog"
+	BASENAME=`basename $0`
+	echo "Usage: ${BASENAME} [SNP accession file name] [output directory]"
+	exit 0
+fi
+
+SNPS_NAME=$1
+OUTPUT_DIR=$2
+
 # Download the GWAS catalog
-if [ ! -f gwas_associations.tsv ]; then
-	wget https://www.ebi.ac.uk/gwas/api/search/downloads/full
-	mv full gwas_associations.tsv
+ASSOCIATIONS=${OUTPUT_DIR}/gwas_associations.tsv
+if [ ! -f ${ASSOCIATIONS}]; then
+	wget -P ${OUTPUT_DIR} https://www.ebi.ac.uk/gwas/api/search/downloads/full
+	mv ${OUTPUT_DIR}/full ${ASSOCIATIONS}
 fi
 
 # Extract the accessions of all SNPs (currently with accession starting with 'rs') that are marked as integenic
-if [ ! -f gwas_snp_accessions.txt ]; then
-	awk -F'\t' '{ if ($26=='1') {print $22} }' gwas_associations.tsv | grep -o "rs[0-9]*" > gwas_snp_accessions.txt
+SNPS=${OUTPUT_DIR}/${SNPS_NAME}
+if [ ! -f ${SNPS} ]; then
+	awk -F'\t' '{ if ($26=='1') {print $22} }' ${ASSOCIATIONS} | grep -o "rs[0-9]*" > ${SNPS}
 fi
