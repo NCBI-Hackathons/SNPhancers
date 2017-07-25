2c4a4b819bd73c4777beaf4376c8d43b5a87b8e0
Sean La <seanla1996@gmail.com>
2017-07-25 15:32:13 -0400

    added script to retrieve enhancer accessions

---
+++ src/get_enhancer_accesions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+	echo "Description: Retrieves accessions for enhancer regulatory regions within the human genome."
+	BASENAME=`basename $0`
+	echo "Usage: ${BASENAME} [output path]"
+	exit 0
+fi
+
+OUTPUT=$1
+
+esearch -query 'regulatory enhancer[feature key] AND PRJNA343958[bioproject]' -db nucleotide -source refseq -organism human |\
+esummary | xtract -pattern DocumentSummary -element Caption > ${OUTPUT}
